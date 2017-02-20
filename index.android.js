04f27b8ef9d5c49b3c3a7be1261234b051436bac
songxiaoliang <563609104@qq.com>
2017-02-20 14:35:03 +0800

    add communication.

--- index.android.js
+++ index.android.js
@@ -4,16 +4,70 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  Image,
+  View,
+  NativeModules,
+  ToastAndroid,
+  DeviceEventEmitter
 } from 'react-native';
 
+let title = 'React Native界面';
 export default class HotRN extends Component {
+
+   /**
+    * 接收原生调用
+    */
+   componentDidMount() {
+       DeviceEventEmitter.addListener('nativeCallRn',(msg)=>{
+            title = "React Native界面,收到数据：" + msg;
+            ToastAndroid.show("发送成功", ToastAndroid.SHORT);
+       })
+   }
+
+   /**
+    * 调用原生代码
+    */
+    skipNativeCall() {
+       let phone = '18637070949';
+       NativeModules.commModule.rnCallNative(phone);
+    }
+
+   /**
+    * Callback 通信方式
+    */
+    callbackComm(msg) {
+        NativeModules.commModule.rnCallNativeFromCallback(msg,(result) => {
+             ToastAndroid.show("CallBack收到消息:" + result, ToastAndroid.SHORT);
+        })
+    }
+
+    /**
+     * Promise 通信方式
+     */
+    promiseComm(msg) {
+        NativeModules.commModule.rnCallNativeFromPromise(msg).then(
+            (result) =>{
+                ToastAndroid.show("Promise收到消息:" + result, ToastAndroid.SHORT)
+            }
+        ).catch((error) =>{console.log(error)});
+    }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          这是React Native 界面!
+        <Text style={styles.welcome} >
+            {title}
         </Text>
+         <Text style={styles.welcome} onPress={this.skipNativeCall.bind(this)}>
+            跳转到拨号界面
+         </Text>
+         <Text style={styles.welcome} onPress={this.callbackComm.bind(this,'callback发送啦')}>
+            Callback通信方式
+         </Text>
+         <Text style={styles.welcome} onPress={this.promiseComm.bind(this,'promise发送啦')}>
+            Promise通信方式
+         </Text>
+         <Image source={require('./images/ic.png')} />
       </View>
     );
   }
